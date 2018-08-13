db36a5fac5c70691432903b7e7d75e283c437661
James Enejo <jamesenejo@James-Enejo.local>
2018-08-13 19:47:59 +0100

    initial setup

---
+++ webpack.config.dev.js
@@ -0,0 +1,23 @@
+import path from 'path';
+
+export default {
+    entry: path.join(__dirname, '/client/index.js'),
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+};
