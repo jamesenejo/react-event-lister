4aa63d095ba895489607be3eeea78959c50b027d
James Enejo <james.enejo@andela.com>
2018-08-14 20:14:47 +0100

    add dummy data for users

---
+++ server/models/users.js
@@ -0,0 +1,25 @@
+const users = [
+    {
+        id: 1,
+        firstname: 'John',
+        lastname: 'Mikel',
+        email: 'john@mikel.com',
+        password: 'pass'
+    },
+    {
+        id: 2,
+        firstname: 'Sarah',
+        lastname: 'Benard',
+        email: 'sarah@ben.com',
+        password: 'passed'
+    },
+    {
+        id: 3,
+        firstname: 'Elena',
+        lastname: 'James',
+        email: 'elena@james.com',
+        password: 'pass'
+    }
+];
+
+export default users;
