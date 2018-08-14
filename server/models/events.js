f17404bba02a2a83ea155016e23b67b1aad3d0ed
James Enejo <james.enejo@andela.com>
2018-08-14 20:34:01 +0100

    update dummy data: add status property

--- server/models/events.js
+++ server/models/events.js
@@ -3,25 +3,29 @@ const events = [
         id: 1,
         type: 'Birthday party',
         venue: 'Eko Hotel',
-        time: '10:00am'
+        time: '10:00am',
+        status: 'Published'
     },
     {
         id: 1,
         type: 'Naming Ceremony',
         venue: 'Eko Hotel',
-        time: '10:00am'
+        time: '10:00am',
+        status: 'Published'
     },
     {
         id: 1,
         type: 'Launching',
         venue: 'Eko Hotel',
-        time: '10:00am'
+        time: '10:00am',
+        status: 'Unpublished'
     },
     {
         id: 1,
         type: 'Wedding',
         venue: 'Eko Hotel',
-        time: '10:00am'
+        time: '10:00am',
+        status: 'Unpublished'
     },
 ];
 
