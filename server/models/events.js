83056140ea4f64da67e4431e4366510f84bad91f
James Enejo <james.enejo@andela.com>
2018-08-14 20:14:25 +0100

    add dummy data for events

---
+++ server/models/events.js
@@ -0,0 +1,28 @@
+const events = [
+    {
+        id: 1,
+        type: 'Birthday party',
+        venue: 'Eko Hotel',
+        time: '10:00am'
+    },
+    {
+        id: 1,
+        type: 'Naming Ceremony',
+        venue: 'Eko Hotel',
+        time: '10:00am'
+    },
+    {
+        id: 1,
+        type: 'Launching',
+        venue: 'Eko Hotel',
+        time: '10:00am'
+    },
+    {
+        id: 1,
+        type: 'Wedding',
+        venue: 'Eko Hotel',
+        time: '10:00am'
+    },
+];
+
+export default events;
