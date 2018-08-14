c5b1472a41536f656e01d6a3c6f877b7b7993907
James Enejo <james.enejo@andela.com>
2018-08-14 20:58:56 +0100

    add controller to get all events

---
+++ server/controllers/listController.js
@@ -0,0 +1,20 @@
+import events from '../models/events';
+import reusables from '../reusables';
+
+const { sendResponse } = reusables;
+
+const listController = {
+    getAllEvents: (req, res) => {
+        const allPublished = [];
+
+        events.forEach((publishedEvent) => {
+            if (publishedEvent.status === 'Published') {
+                allPublished.push(publishedEvent);
+            }
+        });
+
+        return sendResponse(res, 200, `Found ${allPublished.length} events`);
+    }
+};
+
+export default listController;
